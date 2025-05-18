c5825a2475de3b9ecb434dfe83a4ea86f925c2c7
EMMMABK <adilbikiev2@gmail.com>
2025-05-18 18:01:18 +0600

    add grass texture

--- minecraft.py
+++ minecraft.py
@@ -8,26 +8,18 @@ app = Ursina()
 
 player = FirstPersonController()
 
+player.cursor.scale = False       
+player.cursor.color = color.pink  
+
 Sky()
-
-# Флаг, чтобы изменить курсор только один раз
-cursor_initialized = False
-
-def update():
-    global cursor_initialized
-    if not cursor_initialized and hasattr(player, 'cursor'):
-        player.cursor.scale = 0.01
-        player.cursor.color = color.black66
-        cursor_initialized = True  # Не повторять
 
 # Генерация земли
 for i in range(20):
     for j in range(20):
         Button(
-            color=color.white,
             model='cube',
             position=(j, 0, i),
-            texture='white_cube',
+            texture='grass.png',
             parent=scene,
             origin_y=0.5
         )
