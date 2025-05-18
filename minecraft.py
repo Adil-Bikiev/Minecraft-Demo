5f2dddcd007c64140a9e1c7ac237cd0377421e92
EMMMABK <adilbikiev2@gmail.com>
2025-05-18 17:23:57 +0600

    add 5-th commit

--- minecraft.py
+++ minecraft.py
@@ -1,14 +1,25 @@
+from panda3d.core import loadPrcFileData
+loadPrcFileData('', 'gl-version 2 1')  # Совместимость с GLSL на macOS
+
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
+
 app = Ursina()
+
 player = FirstPersonController()
 Sky()
 
 boxes = []
 for i in range(20):
     for j in range(20):
-        box = Button(color=color.white, model='cube', position=(j,0,1),
-            texture='grass.png', parent=scene, origin_y=0.5)
+        box = Button(
+            color=color.white,
+            model='cube',
+            position=(j, 0, i),
+            texture='white_cube',  # Используем встроенную текстуру
+            parent=scene,
+            origin_y=0.5
+        )
         boxes.append(box)
 
 app.run()
